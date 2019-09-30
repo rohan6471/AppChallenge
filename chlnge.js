56e577cdf2b4be0da9d1e90cfd5c3c80e816ffd0
rohan <rohan6471@gmail.com>
2019-09-30 12:47:05 -0500

    update index.html

--- chlnge.js
+++ chlnge.js
@@ -1,17 +1,8 @@
-function calculate() {
-    const string1 = document.getElementById("string1").value
-    const s1=string1.length;
-    const string2 = document.getElementById("string2").value
-    const s2=string2.length;
-    const string3 = document.getElementById("string3").value
-    const s3=string3.length;
-    const result=Math.min(s1,s2,s3);
-    if(result==s1)
-    {
-        document.querySelector("#result").innerHTML="string1 "
-    }
-
-    
+const calc = (radius) => {
+    return Math.PI * radius * radius;
 }
-
-document.querySelector('#calculate').addEventListener('click', calculate)
+document.querySelector('#calculate').addEventListener('click', () => {
+    const radius = parseInt(document.querySelector("#radius").value)
+    const result = `${calc(radius)}`
+    document.querySelector("#result").innerHTML = result;
+})  
